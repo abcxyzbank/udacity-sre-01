b874c271e9a0b3bd8e4d27a93b11cd6983c25d90
DELETE_ME <92442070+abcxyzbank@users.noreply.github.com>
2024-06-04 00:24:41 +0700

    Update _config.tf

--- starter/terraform/_config.tf
+++ starter/terraform/_config.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "udacity-terraform-course1"
+    bucket = "udacity-dev-east-sre-terraform-state"
     key    = "terraform/terraform.tfstate"
     region = "us-east-2"
   }
